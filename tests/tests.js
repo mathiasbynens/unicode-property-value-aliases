ce9554d08868e1244127e2f925641c7a45d36586
Mathias Bynens <mathias@qiwi.be>
2019-03-05 12:50:37 +0100

    Update per Unicode v12.0.0

--- tests/tests.js
+++ tests/tests.js
@@ -1,7 +1,7 @@
 import test from 'ava';
 import propertyValueAliases from '../index.js';
 
-test(t => {
+test('unicode-property-value-aliases', t => {
 	t.is(
 		propertyValueAliases.get('ASCII').get('N'),
 		'No'
